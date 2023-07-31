247487a049a2383500ee7778c1f032ac0854b998
fatcat4096 <140923244+fatcat4096@users.noreply.github.com>
2023-07-31 01:31:09 -0700

    Change pivot table versions to HTML, first run at heat map coloring

--- msf2csv.py
+++ msf2csv.py
@@ -36,10 +36,10 @@ def main():
 	Output:
 		Currently, the script creates multiple output files. Keep or delete whichever you want.
 			1. One copy of the CSV in the original format which used to be downloadable from MSF.gg
-			2. One version of a pivot table showing Power for each character for each of the Alliance members
-			3. One version of a pivot table showing Power and Gear Tier (for planning Gamma 4.5, for example)
-			4. One version of a pivot table showing Power and ISO (for planning Incursion Raids)
-			5. One version with all three data points.
+			2. HTML for a table showing Power for each character for each of the Alliance members
+			3. HTML for a table showing Power and Gear Tier (for planning Gamma 4.5, for example)
+			4. HTML for a table showing Power and ISO (for planning Incursion Raids)
+			5. HTML for a version with all three data points.
 			
 	"""
 	quiet   = 0
@@ -47,10 +47,8 @@ def main():
 
 	alliance_name = 'SIGMA_Infamously_Strange'
 	processed_players = {}
-
-	# FUTURE FEATURE: char_stats - build up a dict with all the char names and min/max for each stat as we find them in the MHTML. We will eventually use these stats for Heat Maps / Coloring in the HTML.
-	# FUTURE FEATURE: char_list shouldn't change from one player to the next. So after all MHTML processed, pull the char_list from char_stats.keys and sort it to use everywhere.
-
+	char_stats = {}
+	
 	# We will look for MHTML in the same directory as this file.
 	try:
 		path = os.path.dirname(__file__)+os.sep
@@ -90,7 +88,7 @@ def main():
 					sys.stderr.write("Parsing %s to %s, %d bytes...\n" % (content_type, file_path, len(part.get_payload())))
 				soup = BeautifulSoup(part.get_payload(decode=True), 'html.parser')
 
-				player_name = soup.find('div', attrs = {'class':'player-name is-italic'}).text.strip().capitalize()
+				player_name = soup.find('div', attrs = {'class':'player-name is-italic'}).text.strip().title()
 
 				#Skip file if player info already processed.
 				if player_name in processed_players:
@@ -113,14 +111,20 @@ def main():
 					
 					# Stats available only if character is recruited.
 					if toon_stats:
+						# Decode Level and Power
 						stats = toon_stats.findAll('div', attrs = {'class':''})
 						level = stats[0].text.strip().split()[1]
 						power = ''.join(stats[1].text.strip().split(','))
+
+						set_min_max(char_stats,char_name,'level',level)
+						set_min_max(char_stats,char_name,'power',power)
 						
+						# Decode Yellow and Red Stars
 						stars = str(toon_stats.find('span'))
 						redStars = str(stars.count('red'))
 						yelStars = str(stars.count('red') + stars.count('orange'))
 						
+						# Decode Abilities
 						abilities = toon_stats.findAll('div', attrs = {'class':'ability-level'})
 						basic   = str(abilities[0]).split('-')[3][1]
 						special = str(abilities[1]).split('-')[3][1]
@@ -129,9 +133,13 @@ def main():
 							ult = str(abilities[-2]).split('-')[3][1]
 						passive = str(abilities[-1]).split('-')[3][1]
 						
+						# Decode Gear Tier
 						gear = char.find('div',attrs={'class':'gear-tier-ring'})
 						tier = str(gear).split('"g')[2].split('"')[0]
+
+						set_min_max(char_stats,char_name,'tier',tier)
 					
+						# Decode ISO Level
 						iso_info = str(char.find('div',attrs={'class','iso-wrapper'}))
 						iso = 0
 						if iso_info.find('-pips-') != -1:
@@ -139,6 +147,8 @@ def main():
 						if iso_info.find('blue') != -1:
 							iso += 5
 						iso = str(iso)
+
+						set_min_max(char_stats,char_name,'iso',iso)
 
 						processed_chars[char_name] = {'level':level,'power':power,'tier':tier,'iso':iso, 'yelStars':yelStars, 'redStars':redStars, 'basic':basic, 'special':special, 'ult':ult, 'passive':passive}
 
@@ -149,71 +159,128 @@ def main():
 					# Add these chars to our list of processed players.
 					processed_players[player_name] = processed_chars
 
+	# Pull char_list from char_stats.
+	char_list = list(char_stats.keys())
+	char_list.sort()	
+
+	filename = path+alliance_name+datetime.datetime.now().strftime("-%Y%m%d-%H%M-")
+
 	# Write the basic output to a CSV in the local directory.
 	keys = ['level','power','tier','iso','yelStars','redStars','basic','special','ult','passive']
-	csv_file = ['Player,Character,']+','.join([key.capitalize() for key in keys])]
+	csv_file = ['Player,Character,'+','.join([key.title() for key in keys])]
 	player_list = list(processed_players.keys())
 	player_list.sort()
 	for player_name in player_list:
 		processed_chars = processed_players[player_name]
-		char_list = list(processed_chars.keys())
-		char_list.sort()
 		for char_name in char_list:
 			csv_file.append(','.join([player_name, char_name] + [processed_chars[char_name][key] for key in keys]))
 
 	# Original CSV format, all info included. 
-	open(path+alliance_name+datetime.datetime.now().strftime("-orig-%Y%m%d-%H%M.csv"), 'w').write('\n'.join(csv_file))
+	open(filename+"orig.csv"), 'w').write('\n'.join(csv_file))
 
 	# Pivot table with Power only. 
-	csv_file = create_pivot_table(processed_players)
-	open(path+alliance_name+datetime.datetime.now().strftime("-pivot-power-%Y%m%d-%H%M.csv"), 'w').write('\n'.join(csv_file))
+	html_file = create_pivot_table(processed_players,char_stats)
+	open(filename+"pivot-power.html"), 'w').write(html_file)
 
 	# Incursion focused pivot table with Power and ISO. 
-	csv_file = create_pivot_table(processed_players,['power','iso'])
-	open(path+alliance_name+datetime.datetime.now().strftime("-pivot-incursion-%Y%m%d-%H%M.csv"), 'w').write('\n'.join(csv_file))
+	html_file = create_pivot_table(processed_players,char_stats,['power','iso'])
+	open(filename+"pivot-incursion.html"), 'w').write(html_file)
 
 	# Gamma focused pivot table with Power and Gear Tier. 
-	csv_file = create_pivot_table(processed_players,['power','tier'])
-	open(path+alliance_name+datetime.datetime.now().strftime("-pivot-gamma-%Y%m%d-%H%M.csv"), 'w').write('\n'.join(csv_file))
+	html_file = create_pivot_table(processed_players,char_stats,['power','tier'])
+	open(filename+"pivot-gamma.html"), 'w').write(html_file)
 
 	# Full pivot table with all three. 
-	csv_file = create_pivot_table(processed_players,['power','tier','iso'])
-	open(path+alliance_name+datetime.datetime.now().strftime("-pivot-all-%Y%m%d-%H%M.csv"), 'w').write('\n'.join(csv_file))
+	html_file = create_pivot_table(processed_players,char_stats,['power','tier','iso'])
+	open(filename+"pivot-all.html"), 'w').write(html_file)
 
 
+def set_min_max(char_stats,char_name,stat,value):
+	value = int(value)
+	char_stats.setdefault(char_name,{})
+	char_stats[char_name].setdefault(stat,{'min':[value-1,0][stat=='iso'],'max':value})
+	if value<char_stats[char_name][stat]['min']:
+		char_stats[char_name][stat]['min'] = value
+	if value>char_stats[char_name][stat]['max']:
+		char_stats[char_name][stat]['max'] = value
 
-def create_pivot_table(processed_players, keys=['power']):
 
-	# FUTURE FEATURE: Convert this output to HTML with clean formating and colspan for char_names across the top of multiple columns if multiple keys provided.
-	# FUTURE FEATURE: Allow filters to be passed in and character inclusion to be based upon whether a character has any of those tags. 
-	# FUTURE FEATURE: Define Lanes and Sections that can be processed individually, using the filters. So requesting a type='gamma' raid would generate four lanes with filters for each section. Incursion would generate one lane, with filters for each origin matching each section.
-	# FUTURE FEATURE: After one each run through the files provided, output a text file named alliance_members.txt which has a list of all the members and definitions for Strike Teams 1-3. If edited to place those members in specific Strike Teams, those Strike teams should be used to group output rows.
+def get_value_color(char_stats,char_name,stat,value):
+	value = int(value)
+	char_stats.setdefault(char_name,{})
+	char_stats[char_name].setdefault(stat,{'min':value-1,'max':value})
 
-	# Write an Power-only pivot-table presentation to a CSV in the local directory.	
+	if not value:
+		return 'White'
+
+	# Linear gradient from red, to yellow, to green.
+	color_scale = ['#ff2d00', '#fe3100', '#fe3500', '#fe3900', '#fe3e00', '#fe4200', '#fe4600', '#fd4b00', '#fd4f00', '#fd5300', '#fd5700', '#fd5c00', '#fd6000', '#fc6400', '#fc6900', '#fc6d00', '#fc7100', '#fc7500', '#fc7a00', '#fb7e00', '#fb8200', '#fb8700', '#fb8b00', '#fb8f00', '#fb9300', '#fa9800', '#fa9c00', '#faa000', '#faa500', '#faa900', '#faad00', '#f9b100', '#f9b600', '#f9ba00', '#f9be00', '#f9c300', '#f9c700', '#f8cb00', '#f8cf00', '#f8d400', '#f8d800', '#f8dc00', '#f8e100', '#f7e500', '#f7e900', '#f7ed00', '#f7f200', '#f7f600', '#f7fa00', '#f7ff00', '#f7ff00', '#f2fe00', '#eefd00', '#eafc01', '#e5fb01', '#e1fa01', '#ddfa02', '#d9f902', '#d4f803', '#d0f703', '#ccf603', '#c8f604', '#c3f504', '#bff405', '#bbf305', '#b7f205', '#b2f106', '#aef106', '#aaf006', '#a5ef07', '#a1ee07', '#9ded08', '#99ed08', '#94ec08', '#90eb09', '#8cea09', '#88e90a', '#83e80a', '#7fe80a', '#7be70b', '#77e60b', '#72e50c', '#6ee40c', '#6ae40c', '#65e30d', '#61e20d', '#5de10d', '#59e00e', '#54df0e', '#50df0f', '#4cde0f', '#48dd0f', '#43dc10', '#3fdb10', '#3bdb11', '#37da11', '#32d911', '#2ed812', '#2ad712', '#26d713', '#26d713']
+
+	min = char_stats[char_name][stat]['min']
+	max = char_stats[char_name][stat]['max']
+
+	color_percent = int((value-min)/(max-min)*100)
+	#print (char_name, char_stats[char_name], max, min, value, color_percent)
+	return color_scale[color_percent]
+	
+
+def create_pivot_table(processed_players, char_stats, keys=['power']):
+
+	# FUTURE: Allow filters to be passed in and character inclusion to be based upon whether a character has any of those tags. 
+	# FUTURE: Define Lanes and Sections that can be processed individually, using the filters. So requesting a type='gamma' raid would generate four lanes with filters for each section. Incursion would generate one lane, with filters for each origin matching each section.
+	# FUTURE: After one each run through the files provided, output a text file named alliance_members.txt which has a list of all the members and definitions for Strike Teams 1-3. If edited to place those members in specific Strike Teams, those Strike teams should be used to group output rows.
+
+	# Pull char_list from char_stats
+	char_list = list(char_stats.keys())
+	char_list.sort()	
+
+	# Get the list of Alliance Members we will iterate through as rows.	
 	player_list = list(processed_players.keys())
 	player_list.sort()
 
-	# Same char_list should be present in every entry. 
-	processed_chars = processed_players[player_list[0]]
-
-	char_list = list(processed_chars.keys())
-	char_list.sort()
-	
 	# Write the top lines - char list and then value descriptors
-	csv_file  = [','.join([''] + [ x+','*(len(keys)-1) for x in char_list])]
+	html_file  = '<table border="1" class="dataframe" style="font-family:verdana">\n'
+	html_file += '  <thead>\n'
+	html_file += '    <tr style="text-align: center;">\n'
+	html_file += '      <th style="background-color:LightBlue;">'+['Alliance member',''][len(keys)>1]+'</th>\n'
+
+	for char in char_list:
+		html_file += '      <th style="background-color:LightBlue;" colspan="'+str(len(keys))+'">'+char+'</th>\n'
+	
+	html_file += '    </tr>\n'
 
 	# Add a line with value descriptors only if more than one item requested.
 	if len(keys)>1:
-		csv_file += [','.join([''] + [key.capitalize() for key in keys]*len(char_list))]
+		html_file += '    <tr style="text-align: center;">\n'
+		html_file += '      <th style="background-color:MidnightBlue;color:White;">Alliance member</th>\n'
+		for char in char_list:
+			for key in keys:
+				html_file += '      <th style="background-color:MidnightBlue;color:white;">'+key.title()+'</th>\n'
+	
+		html_file += '    </tr>\n'
 
-	# write each of the lines, one player per line.
+	html_file += '  </thead>\n'
+	html_file += '  <tbody>\n'
+
+	# Finally, write the data for each row. Player name then relevant stats for each character.
 	for player_name in player_list:
 		processed_chars = processed_players[player_name]
-		csv_file.append(','.join([player_name]+ [processed_chars[char_name][key] for char_name in char_list for key in keys] ))
+		html_file += '    <tr style="text-align: center;">\n'
+		html_file += '     <th style="text-align: left; background-color:LightBlue;">'+player_name+'</th>\n'
 
-	return csv_file
+		for char_name in char_list:
+			for key in keys:
+				html_file += '     <td style="background-color:'+get_value_color(char_stats,char_name,key,processed_chars[char_name][key])+';">'+processed_chars[char_name][key]+'</td>\n'
+	
+		html_file += '     </th>\n'
+		html_file += '    </tr>\n'
+	
+	# Close the HTML table at the end of the doc.
+	html_file += '  </tbody>\n'
+	html_file += '</table >\n'
+		
+	return html_file
 
 
 if __name__ == "__main__":
 	main() # Just run myself
-
