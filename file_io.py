8863a66567fec3a80c21fac06a58b2950d1cf9fe
fatcat4096 <140923244+fatcat4096@users.noreply.github.com>
2026-02-23 04:30:26 +0000

    Add a banner for local_img_cache()

--- file_io.py
+++ file_io.py
@@ -529,6 +529,17 @@ def release_driver(driver):
 
 
 
+#88      .d88888b.   .d8888b.        d8888 888           8888888 888b     d888  .d8888b.        .d8888b.        d8888  .d8888b.  888    888 8888888888 
+#88     d88P" "Y88b d88P  Y88b      d88888 888             888   8888b   d8888 d88P  Y88b      d88P  Y88b      d88888 d88P  Y88b 888    888 888        
+#88     888     888 888    888     d88P888 888             888   88888b.d88888 888    888      888    888     d88P888 888    888 888    888 888        
+#88     888     888 888           d88P 888 888             888   888Y88888P888 888             888           d88P 888 888        8888888888 8888888    
+#88     888     888 888          d88P  888 888             888   888 Y888P 888 888  88888      888          d88P  888 888        888    888 888        
+#88     888     888 888    888  d88P   888 888             888   888  Y8P  888 888    888      888    888  d88P   888 888    888 888    888 888        
+#88     Y88b. .d88P Y88b  d88P d8888888888 888             888   888   "   888 Y88b  d88P      Y88b  d88P d8888888888 Y88b  d88P 888    888 888        
+#8888888 "Y88888P"   "Y8888P" d88P     888 88888888      8888888 888       888  "Y8888P88       "Y8888P" d88P     888  "Y8888P"  888    888 8888888888 
+
+
+
 # Where should assets be downloaded to for local caching?
 asset_cache = f'{os.path.dirname(__file__)}{os.sep}cached_data{os.sep}reports{os.sep}assets{os.sep}'
 if not os.path.exists(asset_cache):
